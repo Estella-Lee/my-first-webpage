0bebc2297fb93e6a747a5cc227b90b290e59be08
Estella Lee <krsd0501@gmail.com>
2021-11-12 12:05:41 +0900

    5-태블릿,모바일 헤더 토글 메뉴 작업 완료 - close #5

--- js/main.js
+++ js/main.js
@@ -1,3 +1,25 @@
+// 모바일 토글 버튼 관련
+const toggleBtn = document.querySelector('.toggleBtn');
+const mainMenu = document.querySelector('.mainMenu');
+const menu = document.querySelector('.gnb');
+
+toggleBtn.addEventListener('click', () => {
+    toggleBtn.classList.toggle('active');
+    mainMenu.classList.toggle('active');
+    menu.classList.toggle('active');
+});
+
+menu.addEventListener('click', removeToggleMenu);
+window.addEventListener('resize', removeToggleMenu);
+
+function removeToggleMenu() {
+    toggleBtn.classList.remove('active');
+    mainMenu.classList.remove('active');
+    menu.classList.remove('active');
+}
+
+
+
 // top 버튼 관련
 const topBtn = document.getElementById('topBtn');
 
